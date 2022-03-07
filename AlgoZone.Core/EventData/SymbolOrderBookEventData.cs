f25cf35436e96c40c615f1d62faabf455f068f47
Bram Hoven <bram.hoven@hotmail.com>
2022-03-07 08:11:26 +0100

    Add queue attribute to specify names

--- AlgoZone.Core/EventData/SymbolOrderBookEventData.cs
+++ AlgoZone.Core/EventData/SymbolOrderBookEventData.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using EasyNetQ;
 
 namespace AlgoZone.Core.EventData
 {
     [Serializable]
+    [Queue(nameof(SymbolOrderBookEventData), ExchangeName = nameof(SymbolOrderBookEventData))]
     public sealed class SymbolOrderBookEventData : SymbolEventData<SymbolOrderBook> { }
 }
--- AlgoZone.Core/EventData/SymbolTickEventData.cs
+++ AlgoZone.Core/EventData/SymbolTickEventData.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using EasyNetQ;
 
 namespace AlgoZone.Core.EventData
 {
     [Serializable]
+    [Queue(nameof(SymbolTickEventData), ExchangeName = nameof(SymbolTickEventData))]
     public class SymbolTickEventData : SymbolEventData<SymbolTick> { }
 }
