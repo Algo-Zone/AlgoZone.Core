04c9f4e78a491d76c2966505b0cc54bb624a66c4
Bram Hoven <bram.hoven@hotmail.com>
2022-04-01 17:24:03 +0200

    Add trading pair event data

---
+++ AlgoZone.Core/EventData/SymbolTradingPair.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AlgoZone.Core.EventData
+{
+    [Serializable]
+    public class SymbolTradingPair : ISymbolData
+    {
+        #region Properties
+
+        public string BaseAsset { get; set; }
+
+        public string QuoteAsset { get; set; }
+
+        public string Status { get; set; }
+
+        /// <inheritdoc />
+        public string Symbol { get; set; }
+
+        #endregion
+    }
+}
---
+++ AlgoZone.Core/EventData/SymbolTradingPairEventData.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using EasyNetQ;
+
+namespace AlgoZone.Core.EventData
+{
+    [Serializable]
+    [Queue(nameof(SymbolTradingPairEventData), ExchangeName = nameof(SymbolTradingPairEventData))]
+    public class SymbolTradingPairEventData : SymbolEventData<SymbolTradingPair> { }
+}
